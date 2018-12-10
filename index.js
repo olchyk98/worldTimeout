0aeae86c8da277bee6038f4bfa9b1da9a8ec1fba
oles <olchyk89@gmail.com>
2018-12-10 14:05:57 +0100

    LAST COMMIT

--- index.js
+++ index.js
@@ -4,30 +4,62 @@ const socketIo = require('socket.io');
 const app = express();
 const server = app.listen(4000, () => console.log("Server is listening on port 4000!"));
 
-const defaultTime = 60;
+const defaultTime = 5,
+	  banTime = 2;
 const info = {
 	time: defaultTime,
 	connected: 0,
-	isEnd: false
+	isEnd: false,
+	banned: [] // id, socket, time
 }
 
-const inti = setInterval(() => {
-	io.sockets.emit('receive_update', --info.time);
-	if(info.time <= 0) {
-		clearInterval(inti);
-		io.sockets.emit('receive_end', true);
-	}
-}, 1000);
+let refreshBan = () => {
+	info.banned.forEach((io, ia) => {
+		io.socket.emit('receive_ban', io.time);
+	});
+}
+let intiModal = null;
+let inti = () => {	
+	clearInterval(intiModal);
+	intiModal = setInterval(function() {
+		io.sockets.emit('receive_update', --info.time);
+		if(info.time <= 0) {
+			clearInterval(this);
+			io.sockets.emit('receive_end', true);
+		}
+		info.banned.forEach((io, ia) => {
+			let a = --io.time;
+			if(!a) {
+				info.banned.splice(ia, 1);
+				io.socket.emit('receive_ban', 0);
+			}
+		});
+		refreshBan();
+	}, 1000);
+}
+
+inti();
 
 const io = socketIo(server);
-
 io.sockets.on('connection', socket => {
 	console.log(`New user connected! id: ${ socket.id }`);
 	info.connected++;
 
+	if(info.time !== 0) socket.emit('receive_update', info.time);
+	else socket.emit('receive_end', true);
 	socket.on('click', data => {
+		if(info.banned.find(io => io.id === socket.id)) return;
+
+		info.banned.push({
+			id: socket.id,
+			socket,
+			time: banTime
+		});
+		refreshBan();
+
 		info.time = defaultTime;
 		io.sockets.emit('receive_update', info.time);
+		inti();
 	});
 });
 
--- public/script.js
+++ public/script.js
@@ -1,12 +1,24 @@
 const socket = io.connect("http://localhost:4000");
 
 let main = document.querySelector('#hitter'),
+	bannedObj = document.querySelector('#banned'),
 	update = data => {
-		main.innerHTML = data;
-	}
+		main.textContent = data;
+	},
+	isBanned = false;
 
 socket.on('receive_update', update);
 socket.on('receive_end', () => {
 	update("END!");
 });
-// socket.emit('click', true);
+socket.on('receive_ban', data => {
+	let a = Boolean(data);
+
+	isBanned = a;
+	bannedObj.textContent = (a) ? `Banned: ${ data }s` : "";
+});
+
+main.addEventListener('click', () => {
+	if(isBanned) return;
+	socket.emit('click', true);
+});
